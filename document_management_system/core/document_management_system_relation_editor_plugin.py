766c2b5827ff57b1f98ab5192224172da34f9732
Denis Rouzaud <denis.rouzaud@gmail.com>
2021-11-03 15:15:23 +0100

    remove unused code

--- document_management_system/core/document_management_system_relation_editor_plugin.py
+++ document_management_system/core/document_management_system_relation_editor_plugin.py
@@ -48,92 +48,11 @@ class DocumentManagementSystemRelationEditorPlugin(QObject):
         self.settingsRegistry = SettingsRegistry()
         QgsApplication.settingsRegistryCore().addSubRegistry(self.settingsRegistry)
 
-    def add_action(
-            self,
-            icon_path,
-            text,
-            callback,
-            enabled_flag=True,
-            add_to_menu=True,
-            add_to_toolbar=True,
-            status_tip=None,
-            whats_this=None,
-            parent=None):
-        """Add a toolbar icon to the toolbar.
-
-        :param icon_path: Path to the icon for this action. Can be a resource
-            path (e.g. ':/plugins/foo/bar.png') or a normal file system path.
-        :type icon_path: str
-
-        :param text: Text that should be shown in menu items for this action.
-        :type text: str
-
-        :param callback: Function to be called when the action is triggered.
-        :type callback: function
-
-        :param enabled_flag: A flag indicating if the action should be enabled
-            by default. Defaults to True.
-        :type enabled_flag: bool
-
-        :param add_to_menu: Flag indicating whether the action should also
-            be added to the menu. Defaults to True.
-        :type add_to_menu: bool
-
-        :param add_to_toolbar: Flag indicating whether the action should also
-            be added to the toolbar. Defaults to True.
-        :type add_to_toolbar: bool
-
-        :param status_tip: Optional text to show in a popup when mouse pointer
-            hovers over the action.
-        :type status_tip: str
-
-        :param parent: Parent widget for the new action. Defaults None.
-        :type parent: QWidget
-
-        :param whats_this: Optional text to show in the status bar when the
-            mouse pointer hovers over the action.
-
-        :returns: The action that was created. Note that the action is also
-            added to self.actions list.
-        :rtype: QAction
-        """
-
-        icon = QIcon(icon_path)
-        action = QAction(icon, text, parent)
-        action.triggered.connect(callback)
-        action.setEnabled(enabled_flag)
-
-        if status_tip is not None:
-            action.setStatusTip(status_tip)
-
-        if whats_this is not None:
-            action.setWhatsThis(whats_this)
-
-        if add_to_toolbar:
-            self.toolbar.addAction(action)
-
-        if add_to_menu:
-            self.interface.addPluginToMenu(
-                self.MENU_ITEM_NAME,
-                action)
-
-        self.actions.append(action)
-
-        return action
-
-    def initGui(self):
-
-        #self.add_action(str(),
-        #                text=self.tr('Configuration &wizard'),
-        #                callback=self.showConfigurationWizard,
-        #                parent=self.interface.mainWindow(),
-        #                add_to_toolbar=False)
-        
+    def initGui(self):        
         QgsGui.relationWidgetRegistry().addRelationWidget(RelationEditorFeatureSideWidgetFactory())
         QgsGui.relationWidgetRegistry().addRelationWidget(RelationEditorDocumentSideWidgetFactory())
 
     def unload(self):
-
         # Removes the plugin menu item
         for action in self.actions:
             self.interface.removePluginMenu(self.MENU_ITEM_NAME,
